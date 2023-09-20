48e748b916a42bf3d681562447ebbd07473a0ea7
Luna <ciubix8514@gmail.com>
2023-09-20 22:30:18 +0300

    add matrix multiplication

--- src/math/mat4x4/mod.rs
+++ src/math/mat4x4/mod.rs
@@ -93,6 +93,78 @@ impl Mat4x4 {
             w: self.m30 * other.x + self.m31 * other.y + self.m32 * other.z + self.m33 * other.w,
         }
     }
+
+    //[1 , 2] . [1 , 2] _ [1 * 1 +  3 * 2,  1 * 2 + 4*2]
+    //[3 , 4]   [3 , 4] -
+    ///Performs matrix multiplication `self` * `other`
+    pub fn multiply(&self, other: Mat4x4) -> Mat4x4 {
+        Mat4x4 {
+            m00: self.m00 * other.m00
+                + self.m01 * other.m10
+                + self.m02 * other.m20
+                + self.m03 * other.m30,
+            m01: self.m00 * other.m01
+                + self.m01 * other.m11
+                + self.m02 * other.m21
+                + self.m03 * other.m31,
+            m02: self.m00 * other.m02
+                + self.m01 * other.m12
+                + self.m02 * other.m22
+                + self.m03 * other.m32,
+            m03: self.m00 * other.m03
+                + self.m01 * other.m13
+                + self.m02 * other.m23
+                + self.m03 * other.m33,
+            m10: self.m10 * other.m00
+                + self.m11 * other.m10
+                + self.m12 * other.m20
+                + self.m13 * other.m30,
+            m11: self.m10 * other.m01
+                + self.m11 * other.m11
+                + self.m12 * other.m21
+                + self.m13 * other.m31,
+            m12: self.m10 * other.m02
+                + self.m11 * other.m12
+                + self.m12 * other.m22
+                + self.m13 * other.m32,
+            m13: self.m10 * other.m03
+                + self.m11 * other.m13
+                + self.m12 * other.m23
+                + self.m13 * other.m33,
+            m20: self.m20 * other.m00
+                + self.m21 * other.m10
+                + self.m22 * other.m20
+                + self.m23 * other.m30,
+            m21: self.m20 * other.m01
+                + self.m21 * other.m11
+                + self.m22 * other.m21
+                + self.m23 * other.m31,
+            m22: self.m20 * other.m02
+                + self.m21 * other.m12
+                + self.m22 * other.m22
+                + self.m23 * other.m32,
+            m23: self.m20 * other.m03
+                + self.m21 * other.m13
+                + self.m22 * other.m23
+                + self.m23 * other.m33,
+            m30: self.m30 * other.m00
+                + self.m31 * other.m10
+                + self.m32 * other.m20
+                + self.m33 * other.m30,
+            m31: self.m30 * other.m01
+                + self.m31 * other.m11
+                + self.m32 * other.m21
+                + self.m33 * other.m31,
+            m32: self.m30 * other.m02
+                + self.m31 * other.m12
+                + self.m32 * other.m22
+                + self.m33 * other.m32,
+            m33: self.m30 * other.m03
+                + self.m31 * other.m13
+                + self.m32 * other.m23
+                + self.m33 * other.m33,
+        }
+    }
 }
 
 impl Mul<f32> for Mat4x4 {
@@ -117,6 +189,22 @@ impl Mul<f32> for Mat4x4 {
             m32: self.m32 * rhs,
             m33: self.m33 * rhs,
         }
+    }
+}
+
+impl Mul<Mat4x4> for Mat4x4 {
+    type Output = Mat4x4;
+
+    fn mul(self, rhs: Mat4x4) -> Self::Output {
+        self.multiply(rhs)
+    }
+}
+
+impl Mul<Vec4> for Mat4x4 {
+    type Output = Vec4;
+
+    fn mul(self, rhs: Vec4) -> Self::Output {
+        self.transform(rhs)
     }
 }
 
@@ -153,4 +241,17 @@ fn test_transformation() {
     let c = a.transform(b);
 
     assert_eq!(c, Vec4::new(30.0, 70.0, 110.0, 150.0));
+}
+
+#[test]
+fn test_mat_mat_mul() {
+    let a = Mat4x4::new(
+        1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0, 13.0, 14.0, 15.0, 16.0,
+    );
+    let o = a.multiply(a);
+    let expected = Mat4x4::new(
+        90.0, 100.0, 110.0, 120.0, 202.0, 228.0, 254.0, 280.0, 314.0, 356.0, 398.0, 440.0, 426.0,
+        484.0, 542.0, 600.0,
+    );
+    assert_eq!(o, expected);
 }
