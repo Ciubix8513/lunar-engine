f8e75df05f152a3f32be79ed2547c1843655fe63
Luna <ciubix8514@gmail.com>
2023-09-23 20:11:11 +0300

    Add transformation matrix

--- src/math/complex_shit.rs
+++ src/math/complex_shit.rs
@@ -1,4 +1,4 @@
-use crate::mat4x4::Mat4x4;
+use crate::{mat4x4::Mat4x4, vec3::Vec3};
 
 pub fn perspercive_projection(
     fov: f32,
@@ -16,4 +16,54 @@ pub fn perspercive_projection(
     Mat4x4::new(
         x_scale, 0.0, 0.0, 0.0, 0.0, y_scale, 0.0, 0.0, 0.0, 0.0, c, 1.0, 0.0, 0.0, d, 0.0,
     )
+}
+
+pub fn scale_matrix(scale: &Vec3) -> Mat4x4 {
+    Mat4x4 {
+        m00: scale.x,
+        m11: scale.y,
+        m22: scale.z,
+        ..Default::default()
+    }
+}
+
+pub fn translation_matrix(translation: &Vec3) -> Mat4x4 {
+    Mat4x4 {
+        m03: translation.x,
+        m13: translation.y,
+        m23: translation.z,
+        ..Default::default()
+    }
+}
+
+pub fn rotation_matrix_euler(rotation: &Vec3) -> Mat4x4 {
+    let sin_x = rotation.x.sin();
+    let cos_x = rotation.x.cos();
+    let sin_y = rotation.y.sin();
+    let cos_y = rotation.y.cos();
+    let sin_z = rotation.z.sin();
+    let cos_z = rotation.z.cos();
+
+    Mat4x4::new(
+        cos_y * cos_z,
+        sin_x * sin_y * cos_z - cos_x * cos_z,
+        cos_x * sin_y * cos_z + sin_x * sin_z,
+        0.0,
+        cos_y * sin_z,
+        sin_x * sin_y * sin_z + cos_x * cos_z,
+        cos_x * sin_y * sin_z - sin_x * cos_z,
+        0.0,
+        -sin_y,
+        sin_x * cos_y,
+        cos_x * cos_y,
+        0.0,
+        0.0,
+        0.0,
+        0.0,
+        1.0,
+    )
+}
+
+pub fn transform_matrix_euler(translation: &Vec3, scale: &Vec3, rotation: &Vec3) -> Mat4x4 {
+    scale_matrix(scale) * rotation_matrix_euler(rotation) * translation_matrix(translation)
 }
