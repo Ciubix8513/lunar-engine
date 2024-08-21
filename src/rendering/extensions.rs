6e1c5472dad8e4cd181c092f4fc87e6272c67148
Luna <ciubix8514@gmail.com>
2024-08-21 17:35:18 +0400

    Fix merge

--- src/rendering/extensions.rs
+++ src/rendering/extensions.rs
@@ -120,7 +120,7 @@ impl Base {
     ///
     ///Everything rendered with this extension will have that color in the parts not occupied by a mesh.
     #[must_use]
-    pub const fn new_with_color(order: u32, color: wgpu::Color) -> Self {
+    pub const fn new_with_color(order: u32, color: Color) -> Self {
         Self {
             priority: order,
             clear_color: color,
