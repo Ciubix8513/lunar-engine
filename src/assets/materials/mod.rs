dcdba4d585c8fbea3e167fde4e3aed340e223d45
Luna <ciubix8514@gmail.com>
2025-04-28 01:03:35 +0400

    Remove old shaders

--- src/assets/materials/mod.rs
+++ src/assets/materials/mod.rs
@@ -1,13 +1,7 @@
-pub use color_lit::ColorLit;
-pub use color_unlit::ColorUnlit;
 pub use lit::Lit;
-pub use texture_unlit::TextureUnlit;
 pub use unlit::Unlit;
 
-mod color_lit;
-mod color_unlit;
 mod lit;
-mod texture_unlit;
 mod unlit;
 
 ///Helper functions for implementing materials
