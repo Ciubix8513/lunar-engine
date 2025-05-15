4886ad9335bb23a0cd67f196e1b1d3392bd1b8a6
Luna <ciubix8514@gmail.com>
2025-05-16 02:29:23 +0400

    Fix the blahaj example

--- examples/blahaj/main.rs
+++ examples/blahaj/main.rs
@@ -16,7 +16,7 @@ use lunar_engine::{
     structures::Color,
     State,
 };
-use lunar_engine_derive::{dependencies, unique_marker_component};
+use lunar_engine_derive::{dependencies, marker_component};
 use winit::keyboard::KeyCode;
 
 #[derive(Default)]
@@ -29,7 +29,7 @@ struct MyState {
     blahaj_mat: u128,
 }
 
-#[unique_marker_component]
+#[marker_component]
 struct Blahaj;
 
 struct Spiny {
